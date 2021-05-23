17e0401e666697d9cc190dd7737477275d52eb3f
jane martin <martin.jane.ellis@gmail.com>
2021-05-22 22:41:02 -0400

    Update ticket list route to handle pagination and updated HTML and CSS to accommodate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import base64
 import requests
 from flask import Flask, render_template
+from requests.exceptions import ConnectionError
 from .config import Configuration
 
 app = Flask(__name__)
@@ -12,16 +13,40 @@ encoded_data = base64.b64encode(
 auth_header = f'Basic {encoded_data}'
 
 
+# @app.route('/', methods=['GET'])
+# def hello_tickets():
+#     """"Route to list of tickets and render ticket list view"""
+#     res = requests.get('https://jemcodes.zendesk.com/api/v2/tickets/',
+#                        headers={'Authorization': auth_header})
+#     if res.status_code == requests.codes.ok:
+#         res_json = res.json()
+#         return render_template('ticket_list.html', res_json=res_json)
+#     else:
+#         return 'Cue the sad trombone sounds - something went wrong!'
+
+
 @app.route('/', methods=['GET'])
 def hello_tickets():
-    """"Route to list of tickets and render ticket list view"""
-    res = requests.get('https://jemcodes.zendesk.com/api/v2/tickets/',
-                       headers={'Authorization': auth_header})
-    if res.status_code == requests.codes.ok:
-        res_json = res.json()
-        return render_template('ticket_list.html', res_json=res_json)
-    else:
-        return 'Cue the sad trombone sounds - something went wrong!'
+    """"Route to list paginated tickets and render ticket list view"""
+    url = 'https://jemcodes.zendesk.com/api/v2/tickets.json?page[size]=100'
+    tickets = []
+
+    while url:
+        try:
+            res = requests.get(url, headers={'Authorization': auth_header})
+            if res.status_code == requests.codes.ok:
+                data = res.json()
+                tickets.extend(data['tickets'])
+                if data['meta']['has_more']:
+                    url = data['links']['next']
+                else:
+                    url = None
+            else:
+                return 'Cue the sad trombone sounds - something went wrong!'
+        except ConnectionError:
+            return 'Zendesk is currently unavailable.'
+
+    return render_template('ticket_list.html', tickets=tickets)
 
 
 @app.route('/<ticket_id>', methods=['GET'])
